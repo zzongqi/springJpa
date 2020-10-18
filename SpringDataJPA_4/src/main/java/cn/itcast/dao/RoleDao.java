4f805b6432554d9448b552ce4767a4fb1d988f89
zzongqi <3031720178@qq.com>
2020-10-18 21:26:58 +0800

    排版梳理

--- SpringDataJPA_4/src/main/java/cn/itcast/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package cn.itcast.dao;
-
-import cn.itcast.domain.Role;
-import cn.itcast.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RoleDao extends JpaRepository<Role,Long> ,JpaSpecificationExecutor<Role> {
-}
--- SpringDataJPA_4/src/main/java/cn/itcast/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.dao;
-
-import cn.itcast.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface UserDao extends JpaRepository<User,Long> ,JpaSpecificationExecutor<User> {
-}
